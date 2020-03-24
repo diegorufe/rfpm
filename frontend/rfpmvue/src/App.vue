e6e7b4cb2ea6ffc58f9e6356aecefbfaebb0f8cc
diegorufe <diegorufe@gmail.com>
2020-03-24 18:09:44 +0100

    Added input number

---
+++ frontend/rfpmvue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <RFPMAppComponent />
+  </div>
+</template>
+
+<script>
+import RFPMAppComponent from "./components/rfpm/components/RFPMAppComponent";
+
+export default {
+  name: "App",
+  components: {
+    RFPMAppComponent
+  }
+};
+</script>
---
+++ frontend/rfpmvue/src/components/rfpm/components/RFPMAppComponent.vue
@@ -0,0 +1,24 @@
+<!--
+
+    Base component for poryect manager application
+
+-->
+
+<script>
+import BaseAppComponent from "../../rfvue/components/base/baseapp/BaseAppComponent";
+
+export default {
+  extends: BaseAppComponent,
+  name: "RFPMAppComponent",
+  props: {},
+  methods: {
+    /**
+     * Method for load configuration for app
+     */
+    loadConfigApp: function() {
+      // Set name for app 
+      this.getVueContext().appName = 'RFPM';
+    }
+  }
+};
+</script>
